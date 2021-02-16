271c0700a118242334be1d4ce491410411d836c5
pandu <lifeisubw@gmail.com>
2021-02-16 18:26:13 +0700

    Finish 9.9: Patientor backend, step2

--- patientor/patientor-backend/build/index.js
+++ patientor/patientor-backend/build/index.js
@@ -3,14 +3,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
+app.use(cors_1.default());
 app.use(express_1.default.json());
-const PORT = 3000;
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+const router = express_1.default.Router();
+router.get('/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+app.use('/api', router);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
